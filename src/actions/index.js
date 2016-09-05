ed424c6c3ee5936fa75a516635dfcf0fada2c952
Eugene Cheltsov <e.cheltsov@msk-wire-macbook-pro-2-e.cheltsov.mail.msk>
2016-09-05 16:48:03 +0300

    cleanup

--- src/actions/index.js
+++ src/actions/index.js
@@ -144,35 +144,6 @@ export const editTodo = fetchAfter((id, text) => {
   })(request, success, failure);
 });
 
-// export function addTodo (text) {
-//   return {
-//     type: types.ADD_TODO,
-//     text
-//   };
-// }
-//
-// export function deleteTodo (id) {
-//   return {
-//     type: types.DELETE_TODO,
-//     id
-//   };
-// }
-//
-// export function editTodo (id, text) {
-//   return {
-//     type: types.EDIT_TODO,
-//     id,
-//     text
-//   };
-// }
-//
-// export function completeTodo (id) {
-//   return {
-//     type: types.COMPLETE_TODO,
-//     id
-//   };
-// }
-//
 // export function completeAll () {
 //   return {
 //     type: types.COMPLETE_ALL
